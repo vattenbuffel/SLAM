ffb74a55dfe4f6a09574ededc77cebe3b9bac568
Noa <nois97.noa@gmail.com>
2022-05-15 21:03:09 +0200

    esp: Add hello world

---
+++ src/esp/src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+
+void setup() {
+Serial.begin(115200);
+Serial.println("hello world\n");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+	Serial.println("hello world\n");
+}
