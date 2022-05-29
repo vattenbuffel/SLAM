794c9b0ba65d73269a0e4b554cf2ba090cc234e3
Noa <nois97.noa@gmail.com>
2022-05-29 13:15:26 +0200

    config: add mqtt broker localhost

--- src/config.py
+++ src/config.py
@@ -1,7 +1,8 @@
 headless = False
 
-mqtt_broker = "192.168.1.11"
+# mqtt_broker = "192.168.1.11"
 # mqtt_broker = "192.168.1.5"
+mqtt_broker = "localhost"
 map_size_m = 15
 map_size_pixels = 250
 map_pub_freq_hz = 1
